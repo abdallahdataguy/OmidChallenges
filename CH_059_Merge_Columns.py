efb5ccadfbc626cf0cd26b41068c86d777d937e4
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-05-29 07:02:20 +0300

    Update CH_059_Merge_Columns.py

--- CH_059_Merge_Columns.py
+++ CH_059_Merge_Columns.py
@@ -8,7 +8,7 @@ file_path = 'CH-059 Merge Columns.xlsx'
 df = pd.read_excel(file_path, usecols='B:J', skiprows=1)
 
 # Perform data wrangling
-cols = range(1, len(df.columns), 2)
+cols = range(1, 9, 2)
 items = [[df.iat[i, 0]] 
          + [sum(df.iloc[i, j : j + 2]) for j in cols] for i in df.index]
 df = pd.DataFrame(items, columns=[df.columns[[0, 1, 3, 5, 7]]])
