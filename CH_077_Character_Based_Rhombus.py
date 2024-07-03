5a919a0becdd047be51b9ec5f38110d7f6cbacb4
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-07-04 01:43:06 +0300

    Update CH_077_Character_Based_Rhombus.py

--- CH_077_Character_Based_Rhombus.py
+++ CH_077_Character_Based_Rhombus.py
@@ -4,7 +4,7 @@
 import pandas as pd
 
 # Create a function to generate the required results
-def triangular(n):
+def char_based_rhombus(n):
     left = list(range(1, n, 2))
     right = list(range(n if n % 2 else n - 1, 0, -2))
     items = left + right
@@ -14,7 +14,7 @@ def triangular(n):
     return values
 
 # Perform data wrangling
-df = pd.DataFrame(triangular(14))
+df = pd.DataFrame(char_based_rhombus(14))
 
 # Display the final output
 df
