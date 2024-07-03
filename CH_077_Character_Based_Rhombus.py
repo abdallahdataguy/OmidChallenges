080a1c480ab59f9711bdd4e12efbf3faec2271ee
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-07-04 02:17:54 +0300

    Update CH_077_Character_Based_Rhombus.py

--- CH_077_Character_Based_Rhombus.py
+++ CH_077_Character_Based_Rhombus.py
@@ -4,17 +4,16 @@
 import pandas as pd
 
 # Create a function to generate the required results
-def char_based_rhombus(n):
-    left = list(range(1, n, 2))
-    right = list(range(n if n % 2 else n - 1, 0, -2))
+def char_based_rhombus(n):        
+    left = list(range(1 if n % 2 else 2, n if n % 2 else n + 1, 2))
+    right = list(range(n, 0 if n % 2 else 1, -2))
     items = left + right
-    values =  {i: [''] * ((n - j) // 2) + ['*'] * j 
-               + [''] * ((n - j) // 2)  
-               for i, j in enumerate(items)}
+    values =  [[''] * ((n - j) // 2) + ['*'] * j 
+               + [''] * ((n - j) // 2)  for j in items]
     return values
 
 # Perform data wrangling
-df = pd.DataFrame(char_based_rhombus(14))
+df = pd.DataFrame(char_based_rhombus(20))
 
 # Display the final output
 df
