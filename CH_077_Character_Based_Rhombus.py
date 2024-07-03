d43e2968ad00b1eaa38a780222f2d629cb83e52e
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-07-04 01:41:51 +0300

    Create CH_077_Character_Based_Rhombus.py

---
+++ CH_077_Character_Based_Rhombus.py
@@ -0,0 +1,20 @@
+# Link to the challenge
+# https://www.linkedin.com/posts/omid-motamedisedeh-74aba166_excelchallenge-powerquerychllenge-excel-activity-7214372344125870081-WYfl/
+
+import pandas as pd
+
+# Create a function to generate the required results
+def triangular(n):
+    left = list(range(1, n, 2))
+    right = list(range(n if n % 2 else n - 1, 0, -2))
+    items = left + right
+    values =  {i: [''] * ((n - j) // 2) + ['*'] * j 
+               + [''] * ((n - j) // 2)  
+               for i, j in enumerate(items)}
+    return values
+
+# Perform data wrangling
+df = pd.DataFrame(triangular(14))
+
+# Display the final output
+df
