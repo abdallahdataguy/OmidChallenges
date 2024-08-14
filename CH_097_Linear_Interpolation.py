0b59867b6c1d1a4409e51f0a9eaf29588d9f80b3
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-08-14 04:13:53 +0300

    Update CH_097_Linear_Interpolation.py

--- CH_097_Linear_Interpolation.py
+++ CH_097_Linear_Interpolation.py
@@ -10,8 +10,8 @@ df = xl("B2:E5", headers=True)
 df2 = pd.DataFrame(data=range(2010, 2023), columns=['Year'])
 df = pd.merge(df2, df, how='left', on='Year')
 df = df.interpolate(method='linear')
-n = len(df)
 
+n = len(df)
 df.loc[0] = 2 * df.loc[1] - df.loc[2]
 df.loc[n-1] = 2 * df.loc[n-2] - df.loc[n-3]
 
